dc80be8f968507be4bf3137f4697624695f6bdf8
gr8kelvin <gr8kelvin@gmail.com>
2024-01-09 05:38:24 -0800

    Input

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""
-Contains the read_file function
-"""
+"""Defines Write_file module."""
 
 
-def read_file(filename=""):
-    """""reads a text file(UTF8) and prints it to stdout"""
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
+    """
     with open(filename, "w", encoding="utf-8") as my_file:
         return my_file.write(text)
